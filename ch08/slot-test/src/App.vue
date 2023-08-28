b2f148a36eb3ba04f46554fff8e6c6ffb16e0050
zeroheat0930 <dongjunkorea@naver.com>
2023-08-28 11:08:54 +0900

    8.2 슬롯

--- ch08/slot-test/src/App.vue
+++ ch08/slot-test/src/App.vue
@@ -2,15 +2,19 @@
   <div>
     <NoSlotTest />
     <SlotTest />
+    <NamedSlotTest />
+    <ScopedSlotTest />
   </div>
 </template>
 
 <script>
 import NoSlotTest from './components/NoSlotTest.vue'
 import SlotTest from './components/SlotTest.vue'
+import NamedSlotTest from './components/NamedSlotTest.vue'
+import ScopedSlotTest from './components/ScopedSlotTest.vue'
 
 export default {
   name: 'App',
-  components: { NoSlotTest, SlotTest }
+  components: { NoSlotTest, SlotTest, NamedSlotTest, ScopedSlotTest }
 }
 </script>
