92766f26a59179004c82ca5551d1715c5741064b
zeroheat0930 <dongjunkorea@naver.com>
2023-08-28 14:49:31 +0900

    8장끝

---
+++ ch08/v-model-test/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <InputName :value="name" @input="$emit('update:name', $event.target.value)" />
+    <h3> 검색어 : {{searchName}}</h3> 
+  </div>
+</template>
+
+<script>
+import InputName from './components/InputName.vue';
+
+export default {
+  name : "App",
+  components : { InputName },
+  data() {
+    return { searchName : "John" }
+  },
+  methods : {
+    changeSearchName(name) {
+      console.log(name)
+      this.searchName = name;
+    }
+  }
+}
+</script>
