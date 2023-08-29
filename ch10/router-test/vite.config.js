df427177b0ee8f7430c4daeeafaf1e8444106ad6
zeroheat0930 <dongjunkorea@naver.com>
2023-08-29 11:36:16 +0900

    빌드할때 페이지 각자나누기

--- ch10/router-test/vite.config.js
+++ ch10/router-test/vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { manualChunksPlugin } from 'vite-plugin-webpackchunkname'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), manualChunksPlugin()
   ],
   resolve: {
     alias: {
