af1ea503073b463ba35efa545ae4be67e8ff4496
zeroheat0930 <dongjunkorea@naver.com>
2023-08-29 10:12:44 +0900

    중첩라우터

--- ch10/router-test/src/router/index.js
+++ ch10/router-test/src/router/index.js
@@ -5,6 +5,7 @@ import About from '@/pages/About.vue'
 import Members from '@/pages/Members.vue'
 import Videos from '@/pages/Videos.vue'
 import MemberInfo from '@/pages/MemberInfo.vue'
+import VideoPlayer from '@/pages/VideoPlayer.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -14,6 +15,12 @@ const router = createRouter({
         { path: '/members', component: Members },
         { path: '/members/:id(\\d+)', component: MemberInfo },
         { path: '/videos', component: Videos },
+        { 
+            path: '/videos', component: Videos, 
+            children : [
+                { path:":id", component: VideoPlayer }
+            ] 
+        },
     ]
 })
 
